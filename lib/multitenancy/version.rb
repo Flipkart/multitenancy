11081f52aeb950ee6e698e4f814a30896ff74152
Mahathi Karnam <venkata.mahathi@flipkart.com>
2014-09-12 20:26:57 +0530

    changed version to 0.0.6.patched

--- lib/multitenancy/version.rb
+++ lib/multitenancy/version.rb
@@ -1,4 +1,4 @@
 module Multitenancy
   # This is a patched version built out of branch disabling_tenant to avoid searching entities with tenant_id(while making select queries)
-  VERSION = "0.0.5.patched"
+  VERSION = "0.0.6.patched"
 end
